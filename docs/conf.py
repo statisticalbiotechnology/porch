33f80a38b12076d5171f678a9cdd1dc41160f200
Lukas Käll <lukas.kall@gmail.com>
2019-11-09 00:17:47 +0100

    d

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../src/'))
-
+import recommonmark
+from recommonmark.transform import AutoStructify
 
 # -- Project information -----------------------------------------------------
 
--- docs/index.md
+++ docs/index.md
@@ -1,10 +1,6 @@
 ## Does this work
 Heho.
 
-API Reference
-=============
-This document is for developers of recommonmark, it contans the API functions
-
 
 Parser Component
 ----------------
