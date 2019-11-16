c35c0f071a092cf94e5fbaa0fc82580dafd77db0
Lukas Käll <lukas.kall@gmail.com>
2019-11-16 10:43:21 +0100

    harmonizing calls

--- porch/porch.py
+++ porch/porch.py
@@ -27,41 +27,34 @@ def porch_single_process(expression_df, phenotype_df, geneset_df,
         tests (list): List of specification of tests that should be performed.
 
     Returns:
-        A tupple of pandas DataFrames results_df, evaluation_df
-        results_df, contains the output of the significance tests
-        evaluation_df, contains the pathway activities
+        results_df, activity_df, untested
+        A pandas DataFrames results_df, containing the output of the significance tests
+        A pandas DataFrames activity_df, containing the pathway activities for each sample
+        untested, a list of the pathway that were not possible to test, due to shortage of data in expression_df.
     """
-    phenotype_df =  phenotype_df[[ col for col in phenotype_df.columns  if col in expression_df.columns ]]
+    phenotype_df =  phenotype_df[[ col for col in phenotype_df.columns  if col in expression_df.columns]]
     expression_df = expression_df[phenotype_df.columns]
-    evaluation_df = phenotype_df.copy()
     results_df = pd.DataFrame()
     set_df = geneset_df[[gene_column, set_column]]
     set_of_all_genes = set(expression_df.index)
-    results,setnames,untested,tested_vars = [],[],[],[]
+    results, setnames, untested, activities, tested_vars = [], [], [], [], None
     for setname, geneset in set_df.groupby([set_column]):
-        print("Processing " + setname, file=sys.stderr)
         genes = list(set(geneset[gene_column].tolist()) & set_of_all_genes)
-        expr = expression_df.loc[genes]
-        expr.dropna(axis=0, how='any', inplace=True)
-        expr = expr.loc[~(expr<=0.0).any(axis=1)]
-        if expr.shape[0]>1:
-            expr_stand = pd.DataFrame(index=expr.index,    columns=expr.columns,data=StandardScaler().fit_transform(np.log(expr.values.T)).T)
-            U, S, Vt = svd(expr_stand.values,full_matrices=False)
-            eigen_genes = (Vt.T)[:,0]
-            evaluation_df.loc["Pathway"] = eigen_genes
-            result, tested_vars = [],[]
-            for test in tests:
-                lm = ols(test, evaluation_df.T).fit()
-                pvals = anova_lm(lm)["PR(>F)"].T.iloc[:-1]
-                result += list(pvals.values)
-                tested_vars += [(test,var) for var in list(pvals.index)]
+        call_args += []
+        setname, result, activity, variables = porch_proc(setname, genes, expression_df, phenotype_df,tests)
+        if result:
             results += [result]
             setnames += [setname]
+            activities += [activity]
+            if not tested_vars:
+                tested_vars = variables
         else:
             untested += [setname]
-    results_cols = pd.MultiIndex.from_tuples(tested_vars,names=["Test","Variable"])
+    results_cols = pd.MultiIndex.from_tuples(tested_vars, names=["Test","Variable"])
     results_df = pd.DataFrame(data=results, columns=results_cols,index=setnames)
-    return results_df, untested
+    activity_df = pd.DataFrame(data=activities, columns=expression_df.columns, index=setnames)
+    return results_df, activity_df, untested
+
 
 def porch(expression_df, phenotype_df, geneset_df,
     gene_column = "gene", set_column = "pathway",
@@ -82,7 +75,7 @@ def porch(expression_df, phenotype_df, geneset_df,
         results_df, activity_df, untested
         A pandas DataFrames results_df, containing the output of the significance tests
         A pandas DataFrames activity_df, containing the pathway activities for each sample
-        untested, a llist of the pathway that were not possible to test, due to shortage of data in expression_df.
+        untested, a list of the pathway that were not possible to test, due to shortage of data in expression_df.
     """
     phenotype_df =  phenotype_df[[ col for col in phenotype_df.columns  if col in expression_df.columns]]
     expression_df = expression_df[phenotype_df.columns]
@@ -111,7 +104,7 @@ def porch(expression_df, phenotype_df, geneset_df,
     return results_df, activity_df, untested
 
 
-def porch_proc(setname, genes, expression_df, phenotype_df,tests):
+def porch_proc(setname, genes, expression_df, phenotype_df,tests,keep_feature_stdv=True):
     """ Core processing node of porch. Takes the analysis from expression values to significance testing. """
     print("Processing " + setname, file=sys.stderr)
     evaluation_df = phenotype_df.copy()
@@ -119,8 +112,10 @@ def porch_proc(setname, genes, expression_df, phenotype_df,tests):
     expr.dropna(axis=0, how='any', inplace=True)
     expr = expr.loc[~(expr<=0.0).any(axis=1)]
     if expr.shape[0]>1:
-        expr_stand = pd.DataFrame(index=expr.index,    columns=expr.columns,data=StandardScaler().fit_transform(np.log(expr.values.T)).T)
-        U, S, Vt = svd(expr_stand.values,full_matrices=False)
+        standardizer = StandardScaler(with_std=keep_feature_stdv)
+        log_data = np.log(expr.values.T)
+        standard_log_data = standardizer.fit_transform(log_data).T
+        U, S, Vt = svd(standard_log_data, full_matrices=False)
         eigen_genes = (Vt.T)[:,0]
         evaluation_df.loc["Pathway"] = eigen_genes
         result, tested_vars = [],[]
